243bc272b0584cf3ec443cf3a5a1b938a4eb11d4
ahmed elkomy <ahmedelkomy@gmail.com>
2024-07-25 01:49:30 +0300

    Create Splash Screen & Enable navigate To Home

---
+++ app/src/main/java/com/route/moviesapp_task/splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.route.moviesapp_task.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.route.moviesapp_task.databinding.ActivitySplashBinding
+import com.route.moviesapp_task.ui.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashBinding? = null
+    val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
