182123422a590203802ad0d7625a97ea0eba1d13
ahmed elkomy <ahmedelkomy@gmail.com>
2024-07-27 09:32:37 +0300

    get  && bind Top Rated List

--- app/src/main/java/com/route/moviesapp_task/ui/fragments/top_rated/TopRatedFragment.kt
+++ app/src/main/java/com/route/moviesapp_task/ui/fragments/top_rated/TopRatedFragment.kt
@@ -4,9 +4,11 @@ package com.route.moviesapp_task.ui.fragments.top_rated
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.route.domain.models.toprated.TopRated
 import com.route.moviesapp_task.R
 import com.route.moviesapp_task.base.fragment.BaseFragment
 import com.route.moviesapp_task.databinding.FragmentTopRatedBinding
+import com.route.moviesapp_task.ui.fragments.popular.adapter.TopRatedAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -14,12 +16,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class TopRatedFragment : BaseFragment<FragmentTopRatedBinding>() {
 
     private val topRatedViewModel: TopRatedViewModel by viewModels<TopRatedViewModel>()
-
+    lateinit var adapter: TopRatedAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecycler()
         binding.lifecycleOwner = this
         topRatedViewModel.getTopRatedList()
         observeLiveData()
+    }
+
+    private fun initRecycler() {
+        adapter = TopRatedAdapter(listOf())
+        binding.rvTopRated.adapter = adapter
     }
 
     override fun observeLiveData() {
@@ -40,9 +48,14 @@ class TopRatedFragment : BaseFragment<FragmentTopRatedBinding>() {
                 negaClick = it.negaClick
             )
         }
-        topRatedViewModel.listOfTopRated.observe(viewLifecycleOwner){
-            
+        topRatedViewModel.listOfTopRated.observe(viewLifecycleOwner) {
+            bindListOfTopRated(it)
         }
+    }
+
+    private fun bindListOfTopRated(list: List<TopRated>) {
+        adapter.updateList(list)
+        binding.rvTopRated.adapter = adapter
     }
 
     override fun getLayout(): Int = R.layout.fragment_top_rated
